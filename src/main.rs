60610ffbf15b4f547b144ef4ae2ea9589d3eba3d
Melody Horn <melody@boringcactus.com>
2019-09-16 15:29:56 -0600

    show public URL

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,9 @@ use uuid::Uuid;
 
 // TODO make this configurable
 const LOG_DIR: &'static str = "/nfs/student/m/mhorn/public_html/ci/554-work";
+const URL_ROOT: &'static str = "https://www.cs.unm.edu/~mhorn/ci/554-work/";
 
-fn run_ci() -> Result<(bool, PathBuf), io::Error> {
+fn run_ci() -> Result<(bool, Uuid), io::Error> {
     // TODO make this configurable
     let command = Command::new("bash")
         .arg("-c")
@@ -26,10 +27,17 @@ fn run_ci() -> Result<(bool, PathBuf), io::Error> {
         .unwrap_or(String::from("???"));
     let final_result = format!("{}\n\nBuild exited with code {}\n", out_text, status_code);
     fs::write(&run_path, &final_result)?;
-    Ok((status.success(), run_path))
+    Ok((status.success(), run_id))
 }
 
 fn main() {
-    let result = run_ci();
-    println!("{:?}", &result);
+    let runner_result = run_ci();
+    match runner_result {
+        Ok((good, id)) => {
+            println!("{} {}{}.txt", good, URL_ROOT, id);
+        }
+        Err(e) => {
+            println!("Error: {}", e);
+        }
+    }
 }
